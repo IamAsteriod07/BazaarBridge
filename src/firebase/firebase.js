9437d9c2ea409f9a59e181eb7a9902471209280c
Abhishek Mallick <127135948+IamAsteriod07@users.noreply.github.com>
2025-09-06 00:57:25 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,27 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDQ8HjDv9S1J_MlzzOQaS5iRjhQyZohU4g",
+  authDomain: "nextapp-2e8c5.firebaseapp.com",
+  projectId: "nextapp-2e8c5",
+  storageBucket: "nextapp-2e8c5.firebasestorage.app",
+  messagingSenderId: "101854660298",
+  appId: "1:101854660298:web:ea95216442b0dbd3341633",
+  measurementId: "G-P89K8HH923"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getFirestore(app);
 export { app, auth, db };
